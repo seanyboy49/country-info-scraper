e8421520d61113e9e18441acd8ec5353217f58f3
sean lee <seanboramlee@gmail.com>
2020-03-09 22:31:56 -0700

    visas

--- visa.js
+++ visa.js
@@ -1,4 +1,8 @@
+const fs = require("fs");
+
 const { fetchData } = require("./utility");
+
+var bracketRegex = /\[([^)]+)\]/;
 
 const url =
   "https://en.wikipedia.org/wiki/Visa_requirements_for_United_States_citizens";
@@ -7,9 +11,50 @@ const getCountryVisas = async () => {
   console.log("start scraping");
 
   const $ = await fetchData(url);
-  const tables = $("table");
+  const tables = $(".wikitable");
 
-  console.log(tables);
+  // Create a write stream
+  const writeStream = fs.createWriteStream("visas.csv");
+  writeStream.write("Country|Visa Requirement|Allowed Stay \n");
+
+  // loop over each table
+  tables.each((index, element) => {
+    const tableRows = $(element).find("tbody tr");
+
+    const filteredRows = tableRows.filter((index, element) => {
+      // remove any tr that contain any th in their children
+      return element.children[1].name !== "th";
+    });
+
+    // loop over filtered rows
+    filteredRows.each((index, element) => {
+      const rowData = $(element).find("td");
+
+      const country = rowData
+        .first()
+        .text()
+        .trim();
+
+      const visaRequirement = rowData
+        .slice(1, 2)
+        .text()
+        .trim();
+
+      const allowedStay = rowData
+        .slice(2, 3)
+        .text()
+        .trim();
+
+      const plainVisaRequirement = visaRequirement.replace(bracketRegex, "");
+      const plainAllowedStay = allowedStay.replace(bracketRegex, "");
+
+      writeStream.write(
+        `${country}|${plainVisaRequirement}|${plainAllowedStay} \n`
+      );
+    });
+  });
+
+  console.log("scraping done");
 };
 
 getCountryVisas();
