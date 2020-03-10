4335a9c4bafeb623c610ad1c5a648479fee606a4
sean lee <seanboramlee@gmail.com>
2020-03-09 22:57:25 -0700

    generate temperatures json

--- temperatures.js
+++ temperatures.js
@@ -5,7 +5,7 @@ const { fetchData } = require("./utility");
 const url =
   "https://en.wikipedia.org/wiki/List_of_cities_by_average_temperature";
 
-const getCountryTemperatures = async () => {
+const getCountryTemperaturesCSV = async () => {
   console.log("start scraping");
 
   const $ = await fetchData(url);
@@ -50,4 +50,55 @@ const getCountryTemperatures = async () => {
   console.log("scraping done");
 };
 
-getCountryTemperatures();
+// getCountryTemperaturesCSV();
+
+const getCountryTemperaturesJSON = async () => {
+  console.log("start scraping");
+
+  const $ = await fetchData(url);
+  const tables = $("table");
+
+  const data = {};
+
+  // Loop over each continent table
+  tables.each((index, element) => {
+    const tableRows = $(element).find("tbody tr");
+
+    // Exclude the first row since it's a header
+    const lineItems = tableRows.slice(1);
+
+    // Iterate over the country/city data
+    lineItems.each((index, element) => {
+      const tableData = $(element).find("td");
+
+      const country = tableData
+        .first()
+        .text()
+        .trim();
+      const city = tableData
+        .slice(1, 2)
+        .text()
+        .trim();
+      const temperaturesText = tableData
+        .slice(2)
+        .text()
+        .trim();
+
+      const farehenheitRegex = /(?<=\()(.*?)(?=\))/g;
+      const farenheitTemperatures = temperaturesText.match(farehenheitRegex);
+
+      if (data[country]) {
+        data[country][city] = farenheitTemperatures;
+      } else {
+        data[country] = {
+          [city]: farenheitTemperatures
+        };
+      }
+    });
+  });
+
+  fs.writeFileSync("temperatures.json", JSON.stringify(data), "utf8");
+  console.log("scraping done");
+};
+
+getCountryTemperaturesJSON();
